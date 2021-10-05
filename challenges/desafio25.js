8ff2b5e825d9a752b3d34925d33a3a690327db24
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-10-05 16:01:12 -0300

    Resolve desafio 25

---
+++ challenges/desafio25.js
@@ -0,0 +1,9 @@
+db.voos.deleteMany(
+  {
+    $and: [
+      { "empresa.nome": "AZUL" },
+      { litrosCombustivel: { $exists: true } },
+      { litrosCombustivel: { $lt: 400 } },
+    ],    
+  },
+);
