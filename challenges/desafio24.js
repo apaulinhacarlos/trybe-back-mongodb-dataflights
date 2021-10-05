c6da6498732e1215c322b68a8c91129abebbeb01
aPaulinhaCarlos <apaulinhacarlos@gmail.com>
2021-10-05 15:48:35 -0300

    Resolve desafio 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -2,11 +2,13 @@ db.voos.findOne(
   {
     $and: [
       { litrosCombustivel: { $exists: true } },
-      { litrosCombustivel: { $lt: 1000 } },
+      { litrosCombustivel: { $lt: 600 } },
+      { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
     ],    
   },
   {
     vooId: true,
+    "empresa.nome": true,
     litrosCombustivel: true,
     _id: false,
   },
